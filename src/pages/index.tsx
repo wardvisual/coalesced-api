7cac670fffff2eeb4d312e87da2cd6650cbb6ce0
Edward <wardvisual@gmail.com>
2022-07-26 08:47:29 +0800

    build: remove storybook packages on this project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  console.log('Nextt');
-  return <h1>Hello-API</h1>;
+  return (
+    <div>
+      <h1>Login Now</h1>
+      <input type="text" />
+      <input type="text" />
+    </div>
+  );
 };
 
 export default Home;
