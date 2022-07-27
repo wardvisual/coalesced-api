ceb7189e2acafe5f91350585205b0b9124c41988
Edward <wardvisual@gmail.com>
2022-07-27 17:30:02 +0800

    chore: create default layout

---
+++ src/shared/layouts/Layout.tsx
@@ -0,0 +1,31 @@
+/* Built-in Libraries */
+import Head from 'next/head';
+import { Fragment } from 'react';
+
+/* Constants */
+import { Application } from '@/shared/constants';
+
+/* Types */
+import { LayoutProps } from '@/shared/types';
+
+/**
+ * The Layout function takes in a title and children props and returns a head component with the title
+ * prop and a main component with the children prop
+ * @param {LayoutProps}  - LayoutProps - This is the interface that we created earlier.
+ * @returns A React component.
+ */
+const Layout = ({ title, children }: LayoutProps) => {
+  return (
+    <Fragment>
+      {/* HEAD */}
+      <Head>
+        <title>{title ? title : Application.TITLE}</title>
+      </Head>
+
+      {/* MAIN */}
+      <main>{children}</main>
+    </Fragment>
+  );
+};
+
+export default Layout;
