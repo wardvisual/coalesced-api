3d06254d50485b3ea119de545f8f57d89629d905
Edward <wardvisual@gmail.com>
2022-07-28 22:46:27 +0800

    refactor: rename layout interfaces

--- src/shared/types/Layout.ts
+++ src/shared/types/Layout.ts
@@ -1,8 +1,8 @@
 import { ReactNode } from 'react';
 
-interface LayoutProps {
+interface ILayout {
   title?: string;
   children: ReactNode;
 }
 
-export default LayoutProps;
+export default ILayout;
