e7092f2b79be50e46070b1984d0b12e4312ee199
Edward <wardvisual@gmail.com>
2022-07-28 22:54:45 +0800

    chore: include theme-provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,24 @@ import type { AppProps } from 'next/app';
 /* Layout */
 import { Layout } from '@/shared/layouts';
 
+/* Theme */
+import { GlobalStyle, theme, ThemeProvider } from '@/shared/theme';
+
+/**
+ * MyApp is a function that takes in a Component and pageProps and returns a Layout component that
+ * contains a ThemeProvider component that contains a GlobalStyle component that contains a Component
+ * component that contains pageProps
+ * @param {AppProps}  - AppProps - this is the props that Next.js passes to the App component.
+ * @returns A Layout component with a ThemeProvider component with a GlobalStyle component with a
+ * Component component with pageProps.
+ */
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
-      <Component {...pageProps} />;
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />;
+      </ThemeProvider>
     </Layout>
   );
 };
