82085b4a385dd69f0c34c47e636b686157ba11f6
Edward <wardvisual@gmail.com>
2022-07-29 20:53:39 +0800

    style: test components

--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -1,13 +1,15 @@
 import { NextPage } from 'next';
+import Image from 'next/image';
 
-import { Button } from '@/shared/components/atoms/Button';
+import banner from '@/assets/svgs/coalesced-art.svg';
 import HomeWrapper from './HomeStyled';
 
 const Home: NextPage = () => {
   return (
     <HomeWrapper>
-      <h1>HOME</h1>
-      <Button text="Click me" />
+      <h1>h111</h1>
+      <button>HEY</button>
+      <Image src={banner} alt="banner" layout="fill" />
     </HomeWrapper>
   );
 };
