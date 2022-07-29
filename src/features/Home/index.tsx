f8d7de34e6ed14738725c13dadb50ae84cd7fabc
Edward <wardvisual@gmail.com>
2022-07-29 12:29:36 +0800

    refactor: update home page jsx

--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -1,7 +1,15 @@
 import { NextPage } from 'next';
 
+import { Button } from '@/shared/components/atoms/Button';
+import HomeWrapper from './HomeStyled';
+
 const Home: NextPage = () => {
-  return <div>Home</div>;
+  return (
+    <HomeWrapper>
+      <h1>HOME</h1>
+      <Button text="Click me" />
+    </HomeWrapper>
+  );
 };
 
 export default Home;
