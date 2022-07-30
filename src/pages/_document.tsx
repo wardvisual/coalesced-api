67ee9b347ca880357c45c0cb03064d6d477495c8
Edward <wardvisual@gmail.com>
2022-07-30 21:00:09 +0800

    chore: use external link to test it is working

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,7 +39,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link
+          {/* <link
             rel="preload"
             href="/public/assets/fonts/Poppins-Regular.woff"
             as="font"
@@ -52,6 +52,10 @@ export default class MyDocument extends Document {
             as="font"
             type="font/woff"
             crossOrigin=""
+          /> */}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
+            rel="stylesheet"
           />
         </Head>
         <body>
