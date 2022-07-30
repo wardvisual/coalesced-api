d45c68c0489a1e43a3faea58e62367d19de78986
Edward <wardvisual@gmail.com>
2022-07-30 21:00:47 +0800

    chore: make some experimentation

--- src/shared/theme/GlobalStyle.ts
+++ src/shared/theme/GlobalStyle.ts
@@ -1,35 +1,33 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-    @font-face {
-        font-family: sansation;
-        src: url('../../../public/assets/fonts/Sansation-Bold-webfont.woff') format('woff');
-        font-weight: normal;
-        font-style: normal;
-    }
+    // @font-face {
+    //     font-family: 'sansation';
+    //     src: url('../../../public/assets/fonts/Sansation-Bold-webfont.woff') format('woff');
+    //     font-weight: normal;
+    //     font-style: normal;
+    //     font-display: swap;
+    // }
     
-    @font-face {
-        font-family: poppins;
-        src: url('../../../public/assets/fonts/Poppins-Regular.woff') format('woff');
-        font-weight: normal;
-        font-style: normal;
-    }
+    // @font-face {
+    //     font-family: 'poppins';
+    //     src: url('../../../public/assets/fonts/Poppins-Regular.woff') format('woff');
+    //     font-weight: normal;
+    //     font-style: normal;
+    //     font-display: swap;
+    // }
     
     *, *::before, *::after {
         padding: 0;
         margin: 0;
         box-sizing: border-box;
-        font-family: poppins;
+        font-family: 'poppins';
         font-weight: normal;
         font-style: normal;
     }
 
     body, html {
         background: #0D0D0D;
-    }
-    
-    button {
-        font-family: 'sansation;
     }
     
     a {
