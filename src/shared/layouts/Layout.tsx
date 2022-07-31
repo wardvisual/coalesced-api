c5b0d57d79d627386f251933997a4667b8f6f992
Edward <wardvisual@gmail.com>
2022-07-31 20:42:56 +0800

    chore: make layout component dynamic

--- src/shared/layouts/Layout.tsx
+++ src/shared/layouts/Layout.tsx
@@ -1,9 +1,12 @@
 /* Built-in Libraries */
 import Head from 'next/head';
-import { Fragment } from 'react';
+import { Fragment, ReactNode } from 'react';
 
 /* Constants */
 import { Application } from '@/shared/constants';
+
+/* Components */
+import { Navbar } from '@/shared/components/molecules/Navbar';
 
 /* Types */
 import { LayoutProps } from '@/shared/types';
@@ -14,18 +17,26 @@ import { LayoutProps } from '@/shared/types';
  * @param {LayoutProps}  - LayoutProps - This is the interface that we created earlier.
  * @returns A React component.
  */
-const Layout = ({ title, children }: LayoutProps) => {
+const Layout = ({ title, root, children }: LayoutProps) => {
+  let renderMeta: ReactNode = <meta content="some content" />;
+  const appTitle: ReactNode = (
+    <title>
+      {title ? `${title} - ${Application.TITLE}` : Application.TITLE}
+    </title>
+  );
+
   return (
     <Fragment>
       {/* HEAD */}
       <Head>
-        <title>
-          {title ? `${title} - ${Application.TITLE}` : Application.TITLE}
-        </title>
+        {appTitle}
+        {root && renderMeta}
       </Head>
 
-      {/* MAIN */}
-      <main>{children}</main>
+      <main className={root ? 'root__layout' : ''}>
+        {root && <Navbar />}
+        {children}
+      </main>
     </Fragment>
   );
 };
